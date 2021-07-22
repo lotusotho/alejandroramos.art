f3dbdd8486b4d24b492083b4d21ba99227e6299f
lotusotho <lotusotho11@gmail.com>
2021-07-22 18:44:06 +0200

    Basic page and Three.js almost completed

--- main.js
+++ main.js
@@ -8,34 +8,82 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
+
+camera.rotation.set(new THREE.Vector3( 0, 0, 0));
+
+//camera.position.setZ(100);
+document.body.onscroll = moveCamera;
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
+    antialias: true,
 });
+
+var mixertable;
+var mixer2Dloc;
+
+scene.background = new THREE.Color( 0x061121 );
+
+var clock = new THREE.Clock();
 
 renderer.setPixelRatio( window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({color: 0xff5797});
-const torus1 = new THREE.Mesh(geometry, material);
+// const geometry = new THREE.TorusGeometry(100, 1, 16, 100);
+// const material = new THREE.MeshStandardMaterial({color: 0xffffff});
+// const torus1 = new THREE.Mesh(geometry, material);
 
 const loader = new GLTFLoader();
 
-loader.load('/models/glTF/mesa.glb', function ( gltf ) {
-    scene.add(gltf.scene);
+// Load 3D models
+const table3D = new THREE.Object3D();
+const litleo3D = new THREE.Object3D();
+const locomocion2D = new THREE.Object3D();
+
+loader.load('/models/glTF/mesa.glb', function ( tablegltf ) {
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    var table = tablegltf.scene;
+    table3D.add(table);
+    mixertable = new THREE.AnimationMixer(tablegltf.scene);
+    var action = mixertable.clipAction( tablegltf.animations[0] );
+    action.play();
+    scene.add(tablegltf.scene);
 }, undefined, function ( error ) {
     console.error( error );
 });
 
-scene.add(torus1);
+loader.load('/models/glTF/litleo.glb', function ( litleogltf ) {
+    var litleo = litleogltf.scene;
+    litleo3D.add(litleo);
+    scene.add(litleogltf.scene);
+}, undefined, function ( error ) {
+    console.error( error );
+});
+
+loader.load('/models/glTF/locomocion2D.glb', function ( locomociongltf ) {
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    var locomocion = locomociongltf.scene;
+    locomocion2D.add(locomocion);
+    locomocion.traverse (function (child) {
+        if (child instanceof THREE.Mesh) {
+            child.frustumCulled = false;
+        }
+    });
+    mixer2Dloc = new THREE.AnimationMixer(locomociongltf.scene);
+    var action = mixer2Dloc.clipAction( locomociongltf.animations[0] );
+    action.play();
+    scene.add(locomociongltf.scene);
+}, undefined, function ( error ) {
+    console.error( error );
+});
+
+//scene.add(torus1);
 
 const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(5,5,5)
+pointLight.position.set(5,5,30)
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
@@ -47,27 +95,57 @@ scene.add(lightHelper, gridHelper);
 const controls =  new OrbitControls(camera, renderer.domElement);
 
 function addStar() {
-    const geometry = new THREE.geometry
+    const geometry = new THREE.SphereGeometry();
     const material = new THREE.MeshBasicMaterial({color: 0xffffff})
     const star = new THREE.Mesh(geometry, material);
 
-    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(200));
+
+    const randomscale = THREE.MathUtils.randFloat(0.2, 1);
 
     star.position.set(x, y, z);
+    star.scale.set(randomscale, randomscale, randomscale);
     scene.add(star)
 }
 
-Array(200).fill().forEach(addStar);
+Array(100).fill().forEach(addStar);
 
-function animate1() {
-    requestAnimationFrame(animate1);
-    torus1.rotation.x += 0.01;
-    torus1.rotation.y += 0.005;
-    torus1.rotation.z += 0.01;
+//Textures
+const spaceTexture = new THREE.TextureLoader().load('/models/textures/mesalayout.png');
 
-    controls.update();
+function animate() {
+    requestAnimationFrame(animate);
+
+    const delta = clock.getDelta();
+
+    mixertable.update( delta );
+
+    mixer2Dloc.update( delta );
 
     renderer.render(scene, camera);
 }
 
-animate1();
+animate();
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    litleo3D.rotation.x += 0.05;
+    litleo3D.rotation.y += 0.075;
+    litleo3D.rotation.z += 0.05;
+
+    camera.position.y = -t * -0.04;
+    camera.position.z = t * -0.0002;
+    camera.position.x = t * -0.0002;
+}
+
+// function animate1() {
+//     requestAnimationFrame(animate1);
+//     torus1.rotation.x += 0.035;
+//     torus1.rotation.y += 0.005;
+//     torus1.rotation.z += 0.01;
+
+    
+//     renderer.render(scene, camera);
+// }
+
+//animate1();
