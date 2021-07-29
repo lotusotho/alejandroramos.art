2d59a428841e3a6a09a7b25225b41b6143fd5e45
lotusotho <lotusotho11@gmail.com>
2021-07-29 19:25:24 +0200

    env headers

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const path = require('path');
 
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+const port = process.env.PORT || 80;
 
 app.get('*', function(req, res) {  
     res.redirect('https://' + req.headers.host + req.url);
 
 })
 
-app.listen(80);
+app.listen(port);
