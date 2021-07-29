1fca96fb25cb4b3fcf65ae50033f18aa521c14c9
lotusotho <lotusotho11@gmail.com>
2021-07-29 19:21:38 +0200

    http to https 2

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+
+app.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+
+})
+
 app.listen(port);
