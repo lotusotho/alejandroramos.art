beeb0357d8343d1fa799919c2c6b6b36d35eb59d
lotusotho <lotusotho11@gmail.com>
2021-07-29 18:58:21 +0200

    http redirect to https

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+// Make sure to run the code using sudo as port 80 needs sudo access 
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(301,{Location: `https://${req.headers.host}${req.url}`});
+  res.end();
+});
+
+server.listen(80);
+console.log(`http2https ==> 80:443`);
