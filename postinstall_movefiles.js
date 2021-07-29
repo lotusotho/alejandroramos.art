cf15108cdd9ce4d46ae3cdc99366872692cb164b
Lotusotho <lotusotho11@gmail.com>
2021-07-29 12:03:58 +0200

    maybe this works maybe

---
+++ postinstall_movefiles.js
@@ -0,0 +1,9 @@
+var fs = require('fs')
+
+var oldPath = '/models'
+var newPath = '/dist/assets/'
+
+fs.rename(oldPath, newPath, function (err) {
+  if (err) throw err
+  console.log('Successfully renamed - AKA moved!')
+})
