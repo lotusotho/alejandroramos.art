047a45f047f6d04d1073d64a25e587d7e15f1d3e
Lotusotho <lotusotho11@gmail.com>
2021-07-30 10:36:24 +0200

    Change server.js back to what it was

--- server.js
+++ server.js
@@ -4,13 +4,5 @@ const path = require('path');
 
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 3000;
-
-app.get('*',function(req,res,next){
-    if(req.headers['x-forwarded-proto']!='https')
-      res.redirect('https://alejandroramos.art'+req.url)
-    else
-      next() /* Continue to other routes if we're not redirecting */
-  })
-
+const port = process.env.PORT || 80;
 app.listen(port);
